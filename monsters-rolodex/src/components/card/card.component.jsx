3d09cfa7855667d4f28c818bda275df290f2316d
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-15 02:14:05 -0600

    added images

--- monsters-rolodex/src/components/card/card.component.jsx
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -4,6 +4,7 @@ import './card.style.css'
 
 export const Card = (props) => (
   <div className="card-container">
+    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=200x200` } />
     <h1> {props.monster.name} </h1>
   </div>
 );
