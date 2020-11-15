f80614df2d098b6dd88fd455452ac2b2e69ef327
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-15 02:15:09 -0600

    align text in cards

--- monsters-rolodex/src/components/card/card.component.jsx
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -4,7 +4,11 @@ import './card.style.css'
 
 export const Card = (props) => (
   <div className="card-container">
-    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=200x200` } />
+    <img
+      alt="monster"
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=200x200`}
+    />
     <h1> {props.monster.name} </h1>
+    <p> {props.monster.email} </p>
   </div>
 );
